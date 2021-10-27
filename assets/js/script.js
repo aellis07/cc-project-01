a99de5c66716dd205f216e080dcae2456a43e258
Anthony Ellis <amellis245@gmail.com>
2021-10-27 09:43:20 -0400

    added sample trending-video code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,46 +10,105 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     });
 };
 
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement("script");
+// ============================================================
+// YOUTUTBE API
+{
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement("script");
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player("youtubeCard", {
-        height: "390",
-        width: "640",
-        videoId: "M7lc1UVf-VE",
-        playerVars: {
-            playsinline: 1,
-        },
-        events: {
-            onReady: onPlayerReady,
-            onStateChange: onPlayerStateChange,
-        },
-    });
-}
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player("youtubeCard", {
+            height: "390",
+            width: "640",
+            videoId: "M7lc1UVf-VE",
+            playerVars: {
+                playsinline: 1,
+            },
+            events: {
+                onReady: onPlayerReady,
+                onStateChange: onPlayerStateChange,
+            },
+        });
+    }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+            setTimeout(stopVideo, 6000);
+            done = true;
+        }
+    }
+    function stopVideo() {
+        player.stopVideo();
     }
 }
-function stopVideo() {
-    player.stopVideo();
+// ===============================================================
+// YOUTUBE TRENDS
+{
+    function authenticate() {
+        return gapi.auth2
+            .getAuthInstance()
+            .signIn({
+                scope: "https://www.googleapis.com/auth/youtube.readonly",
+            })
+            .then(
+                function () {
+                    console.log("Sign-in successful");
+                },
+                function (err) {
+                    console.error("Error signing in", err);
+                }
+            );
+    }
+    function loadClient() {
+        gapi.client.setApiKey("YOUR_API_KEY");
+        return gapi.client
+            .load(
+                "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+            )
+            .then(
+                function () {
+                    console.log("GAPI client loaded for API");
+                },
+                function (err) {
+                    console.error("Error loading GAPI client for API", err);
+                }
+            );
+    }
+    // Make sure the client is loaded and sign-in is complete before calling this method.
+    function execute() {
+        return gapi.client.youtube.videos
+            .list({
+                part: ["snippet,contentDetails,statistics"],
+                chart: "mostPopular",
+                regionCode: "US",
+            })
+            .then(
+                function (response) {
+                    // Handle the results here (response.result has the parsed body).
+                    console.log("Response", response);
+                },
+                function (err) {
+                    console.error("Execute error", err);
+                }
+            );
+    }
+    gapi.load("client:auth2", function () {
+        gapi.auth2.init({ client_id: "YOUR_CLIENT_ID" });
+    });
 }
