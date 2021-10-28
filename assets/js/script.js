a4a9846caecf875db43b1cf0d54b009b8674af00
Syed Bukhari <syedubukhari@gmail.com>
2021-10-28 14:13:39 -0400

    added stocks api url and key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,33 @@
 // GEOLOCATION
 function getGeolocation() {
-	if (navigator.geolocation) {
-		//check if geolocation is available
-		navigator.geolocation.getCurrentPosition(function (position) {
-			console.log(position);
-			// reverse geocoding?
-			// fetch( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
-			//     .then(function(response) {
-			//         return response.json()
-			//     })
-			//     .then(function(data) {
-			//         console.log(data);
-			//     })
-		});
-	}
+    if (navigator.geolocation) {
+        //check if geolocation is available
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position);
+            // reverse geocoding?
+            // fetch( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
+            //     .then(function(response) {
+            //         return response.json()
+            //     })
+            //     .then(function(data) {
+            //         console.log(data);
+            //     })
+        });
+    }
 }
 
 getGeolocation();
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-	const token =
-		"[BQBLAH4n-wN2yiCfoXvMt179Ok1Rn-tEFA-O9bU5RDVSrPKzZ1iWOFbx8OiY_h82AGY4P7YSiNmSxMxT2AyEBX1pDXsDAT3GEMXhpqWBwhvu9J4Xg0jbKCNhKL-nWjF1fcvCAlXOjKfEj2KcYMcWtT34gEErZCTxHHYc7RVk]";
-	const player = new Spotify.Player({
-		name: "Daily Deck Player",
-		getOAuthToken: (cb) => {
-			cb(token);
-		},
-		volume: 0.5,
-	});
+    const token =
+        "[BQBLAH4n-wN2yiCfoXvMt179Ok1Rn-tEFA-O9bU5RDVSrPKzZ1iWOFbx8OiY_h82AGY4P7YSiNmSxMxT2AyEBX1pDXsDAT3GEMXhpqWBwhvu9J4Xg0jbKCNhKL-nWjF1fcvCAlXOjKfEj2KcYMcWtT34gEErZCTxHHYc7RVk]";
+    const player = new Spotify.Player({
+        name: "Daily Deck Player",
+        getOAuthToken: (cb) => {
+            cb(token);
+        },
+        volume: 0.5,
+    });
 };
 
 // ============================================================
@@ -75,3 +75,15 @@ function stopVideo() {
 }
 
 // ===============================================================
+// Stocks API
+
+fetch(
+    "https://financialmodelingprep.com/api/v3/actives?apikey=a0b34495f1b7cdb84c38ee2d58875f0a"
+)
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
