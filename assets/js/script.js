79b0e5feacdf7f5f67c5eb94beafc88f378bb009
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2021-10-28 14:35:30 -0400

    commiting spotify api and ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,13 +143,18 @@ const UIController = (function () {
             detailDiv.innerHTML = "";
 
             const html = `      
-            <div class="genre">
+            <div class="genre" style="
+            display: block;
+            text-align: center;">
                 <label for="Genre">${title}:</label>
             </div>
-            <div class="artist">
+            <div class="artist" style="
+            display: block;
+            text-align: center;">
                 <label for="artist">By ${artist}:</label>
             </div> 
-            <div class="image">
+            <div class="image" style="
+            padding-bottom: 20px;">
             <img src="${img}" alt="">
             </div>  
             `;
@@ -268,36 +273,36 @@ const APPController = (function (UICtrl, APICtrl) {
 // will need to call a method to load the genres on page load
 APPController.init();
 // GEOLOCATION
-function getGeolocation() {
-	if (navigator.geolocation) {
-		//check if geolocation is available
-		navigator.geolocation.getCurrentPosition(function (position) {
-			console.log(position);
-			// reverse geocoding?
-			// fetch( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
-			//     .then(function(response) {
-			//         return response.json()
-			//     })
-			//     .then(function(data) {
-			//         console.log(data);
-			//     })
-		});
-	}
-}
+// function getGeolocation() {
+// 	if (navigator.geolocation) {
+// 		//check if geolocation is available
+// 		navigator.geolocation.getCurrentPosition(function (position) {
+// 			console.log(position);
+// 			// reverse geocoding?
+// 			// fetch( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
+// 			//     .then(function(response) {
+// 			//         return response.json()
+// 			//     })
+// 			//     .then(function(data) {
+// 			//         console.log(data);
+// 			//     })
+// 		});
+// 	}
+// }
 
-getGeolocation();
+// getGeolocation();
 
-window.onSpotifyWebPlaybackSDKReady = () => {
-	const token =
-		"[BQBLAH4n-wN2yiCfoXvMt179Ok1Rn-tEFA-O9bU5RDVSrPKzZ1iWOFbx8OiY_h82AGY4P7YSiNmSxMxT2AyEBX1pDXsDAT3GEMXhpqWBwhvu9J4Xg0jbKCNhKL-nWjF1fcvCAlXOjKfEj2KcYMcWtT34gEErZCTxHHYc7RVk]";
-	const player = new Spotify.Player({
-		name: "Daily Deck Player",
-		getOAuthToken: (cb) => {
-			cb(token);
-		},
-		volume: 0.5,
-	});
-};
+// window.onSpotifyWebPlaybackSDKReady = () => {
+// 	const token =
+// 		"[BQBLAH4n-wN2yiCfoXvMt179Ok1Rn-tEFA-O9bU5RDVSrPKzZ1iWOFbx8OiY_h82AGY4P7YSiNmSxMxT2AyEBX1pDXsDAT3GEMXhpqWBwhvu9J4Xg0jbKCNhKL-nWjF1fcvCAlXOjKfEj2KcYMcWtT34gEErZCTxHHYc7RVk]";
+// 	const player = new Spotify.Player({
+// 		name: "Daily Deck Player",
+// 		getOAuthToken: (cb) => {
+// 			cb(token);
+// 		},
+// 		volume: 0.5,
+// 	});
+// };
 
 // ============================================================
 // YOUTUTBE API
