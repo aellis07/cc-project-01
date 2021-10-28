10832ae6a69d70481ed2df7c571c29d99fc4bc85
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2021-10-28 17:06:13 -0400

    commiting initial news api integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -274,22 +274,22 @@ const APPController = (function (UICtrl, APICtrl) {
 APPController.init();
 // GEOLOCATION
 function getGeolocation() {
-	if (navigator.geolocation) {
-		//check if geolocation is available
-		navigator.geolocation.getCurrentPosition(function (position) {
-			console.log(position);
-			// reverse geocoding?
-			// fetch( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
-			//     .then(function(response) {
-			//         return response.json()
-			//     })
-			//     .then(function(data) {
-			//         console.log(data);
-			//     })
-		});
-	}
-	// check if saved, don't prompt?
-	// button to prompt? (weather)
+    if (navigator.geolocation) {
+        //check if geolocation is available
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position);
+            // reverse geocoding?
+            // fetch( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
+            //     .then(function(response) {
+            //         return response.json()
+            //     })
+            //     .then(function(data) {
+            //         console.log(data);
+            //     })
+        });
+    }
+    // check if saved, don't prompt?
+    // button to prompt? (weather)
 }
 
 // ============================================================
@@ -305,35 +305,43 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // This function creates an <iframe> (and YouTube player) after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-	player = new YT.Player("youtubeCard", {
-		height: "390",
-		width: "640",
-		playerVars: {
-			listType: "playlist",
-			list: "PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-",
-		},
-		events: {
-			onReady: onPlayerReady,
-			onStateChange: onPlayerStateChange,
-		},
-	});
+    player = new YT.Player("youtubeCard", {
+        height: "390",
+        width: "640",
+        playerVars: {
+            listType: "playlist",
+            list: "PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-",
+        },
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange,
+        },
+    });
 }
 
 // The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-	event.target.playVideo();
+    event.target.playVideo();
 }
 
 // 5. The API calls this function when the player's state changes. The function indicates that when playing a video (state=1) the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		setTimeout(stopVideo, 6000);
-		done = true;
-	}
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
 }
 function stopVideo() {
-	player.stopVideo();
+    player.stopVideo();
 }
 
 // ===============================================================
+// News API
+
+fetch(
+    "http://api.mediastack.com/v1/news?access_key=6610298920a05b1cbc9139d1d5b79485&languages=en&countries=us&categories=technology&limit=10"
+)
+    .then((response) => response.json())
+    .then((news) => console.log(news));
+
