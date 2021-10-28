d915fc0f5a874fd555567fda2aa8d8937b532c2c
skg <skaseyg@gmail.com>
2021-10-28 10:29:02 -0400

    added tab title'

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,8 @@ function getGeolocation() {
 			//     })
 		});
 	}
+	// check if saved, don't prompt?
+	// button to prompt? (weather)
 }
 
 getGeolocation();
@@ -43,35 +45,35 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // This function creates an <iframe> (and YouTube player) after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player("youtubeCard", {
-        height: "390",
-        width: "640",
-        playerVars: {
-            listType: "playlist",
-            list: "PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-",
-        },
-        events: {
-            onReady: onPlayerReady,
-            onStateChange: onPlayerStateChange,
-        },
-    });
+	player = new YT.Player("youtubeCard", {
+		height: "390",
+		width: "640",
+		playerVars: {
+			listType: "playlist",
+			list: "PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-",
+		},
+		events: {
+			onReady: onPlayerReady,
+			onStateChange: onPlayerStateChange,
+		},
+	});
 }
 
 // The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-    event.target.playVideo();
+	event.target.playVideo();
 }
 
 // 5. The API calls this function when the player's state changes. The function indicates that when playing a video (state=1) the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+		setTimeout(stopVideo, 6000);
+		done = true;
+	}
 }
 function stopVideo() {
-    player.stopVideo();
+	player.stopVideo();
 }
 
 // ===============================================================
